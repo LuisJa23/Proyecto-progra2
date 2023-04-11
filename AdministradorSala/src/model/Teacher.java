21af224ca0298460a813e9d74d9cc87bd6911389
javierlink22@gmail.com <javierlink22@gmail.com>
2023-04-11 17:02:35 -0500

    logica profesor

--- AdministradorSala/src/model/Teacher.java
+++ AdministradorSala/src/model/Teacher.java
@@ -1,4 +1,60 @@
 package model;
 
-public class Teacher {
+import java.io.Serializable;
+
+public class Teacher implements Serializable {
+
+    private String name;
+    private String id;
+    private String userName;
+    private transient String password;
+
+    public Teacher(String name, String id, String userName, String password) {
+        this.name = name;
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
