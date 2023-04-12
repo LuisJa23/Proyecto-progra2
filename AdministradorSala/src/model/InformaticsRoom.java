bfa699f5a1dc500446995fd801e8ef01adb820bf
javierlink22@gmail.com <javierlink22@gmail.com>
2023-04-12 10:30:16 -0500

    Logica estudiante

---
+++ AdministradorSala/src/model/InformaticsRoom.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+
+public class InformaticsRoom {
+
+    private String id;
+    private ArrayList<Computer> computers;
+    //private ArrayList<Loan> loanHistory;
+
+
+
+    private void addComputer(Computer cumputer){
+
+    }
+}
